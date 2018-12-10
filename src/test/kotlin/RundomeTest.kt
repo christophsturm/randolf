d343c4735b82fc2614803a02d82c9b884abd6726
Christoph Sturm <me@christophsturm.com>
2018-12-10 18:25:05 +0100

    remove boiler plate comments

--- src/test/kotlin/RundomeTest.kt
+++ src/test/kotlin/RundomeTest.kt
@@ -4,17 +4,12 @@ import com.oneeyedmen.minutest.junit.JUnit5Minutests
 import com.oneeyedmen.minutest.junit.context
 import kotlin.reflect.KClass
 
-// this is just the first test from the minutest readme converted to strikt, to check that both work
 class RundomeTest : JUnit5Minutests {
     data class A(val a: Int, val b: Int, val c: B)
     data class B(val a: String)
 
 
-    // The fixture type is the generic type of the test, here Stack<String>
     override val tests = context<B> {
-
-        // The fixture block tells Minutest how to create an instance of the fixture.
-        // Minutest will call it once for every test.
         fixture {
             Rundome.create()
         }
