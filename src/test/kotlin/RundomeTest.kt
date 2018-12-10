52adff8b9d112d812df3d5f11f434e765c04f597
Christoph Sturm <me@christophsturm.com>
2018-12-10 20:43:11 +0100

    don't abuse fixtures

--- src/test/kotlin/RundomeTest.kt
+++ src/test/kotlin/RundomeTest.kt
@@ -1,5 +1,5 @@
-import RundomeTest.A
-import RundomeTest.B
+import RundomeTest.IntFieldsAndNestedDataClassDC
+import RundomeTest.StringDC
 import com.oneeyedmen.minutest.experimental.SKIP
 import com.oneeyedmen.minutest.experimental.skipAndFocus
 import com.oneeyedmen.minutest.junit.JUnit5Minutests
@@ -7,27 +7,21 @@ import com.oneeyedmen.minutest.junit.context
 import kotlin.reflect.KClass
 
 class RundomeTest : JUnit5Minutests {
-    data class A(val a: Int, val b: Int, val c: B)
-    data class B(val a: String)
-    data class C(val a: String)
+    data class StringDC(val a: String)
+    data class IntFieldsAndNestedDataClassDC(val a: Int, val b: Int, val c: StringDC)
+    data class AnotherStringDC(val a: String)
 
 
-    override val tests = context<B>(transform = skipAndFocus) {
-        fixture {
-            Rundome.create()
+    override val tests = context<Unit>(transform = skipAndFocus) {
+        test("can create a data class with a String field") {
+            Rundome.create<StringDC>()
         }
 
-        test("fixture works") {
-            // no idea what i could assert on the object
+        test("can create a data class with a nested data class") {
+            Rundome.create<IntFieldsAndNestedDataClassDC>()
         }
-
-        derivedContext<A>("different type") {
-            deriveFixture { Rundome.create() }
-            test("works") {}
-        }
-        SKIP - derivedContext<C>("another type, currently unsupported") {
-            deriveFixture { Rundome.create() }
-            test("works") {}
+        SKIP - test("can not create anything else because everything is hardcoded") {
+            Rundome.create<AnotherStringDC>()
         }
     }
 }
@@ -37,8 +31,8 @@ object Rundome {
 
     fun create(kClass: KClass<*>): Any {
         return when (kClass) {
-            A::class -> A(1, 2, B("blah"))
-            B::class -> B("blah")
+            IntFieldsAndNestedDataClassDC::class -> IntFieldsAndNestedDataClassDC(1, 2, StringDC("blah"))
+            StringDC::class -> StringDC("blah")
             else -> throw RuntimeException()
         }
     }
