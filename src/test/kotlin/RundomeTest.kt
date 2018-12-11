ca30ad996edb40d3cc9b68a02c0118615c3abdd7
Christoph Sturm <me@christophsturm.com>
2018-12-11 14:47:55 +0100

    implement first working version, thanks to bitkid

--- src/test/kotlin/RundomeTest.kt
+++ src/test/kotlin/RundomeTest.kt
@@ -1,10 +1,10 @@
-import RundomeTest.IntFieldsAndNestedDataClassDC
-import RundomeTest.StringDC
 import com.oneeyedmen.minutest.experimental.SKIP
 import com.oneeyedmen.minutest.experimental.skipAndFocus
 import com.oneeyedmen.minutest.junit.JUnit5Minutests
 import com.oneeyedmen.minutest.junit.context
 import kotlin.reflect.KClass
+import kotlin.reflect.full.createType
+import kotlin.reflect.jvm.javaType
 
 class RundomeTest : JUnit5Minutests {
     data class StringDC(val a: String)
@@ -13,6 +13,9 @@ class RundomeTest : JUnit5Minutests {
 
 
     override val tests = context<Unit>(transform = skipAndFocus) {
+        SKIP - test("can create a minimal object where everything that can be null is null") {
+
+        }
         test("can create a data class with a String field") {
             Rundome.create<StringDC>()
         }
@@ -20,7 +23,7 @@ class RundomeTest : JUnit5Minutests {
         test("can create a data class with a nested data class") {
             Rundome.create<IntFieldsAndNestedDataClassDC>()
         }
-        SKIP - test("can not create anything else because everything is hardcoded") {
+        test("can not create anything else because everything is hardcoded") {
             Rundome.create<AnotherStringDC>()
         }
     }
@@ -30,11 +33,18 @@ object Rundome {
     inline fun <reified T> create(): T = create(T::class) as T
 
     fun create(kClass: KClass<*>): Any {
-        return when (kClass) {
-            IntFieldsAndNestedDataClassDC::class -> IntFieldsAndNestedDataClassDC(1, 2, StringDC("blah"))
-            StringDC::class -> StringDC("blah")
-            else -> throw RuntimeException()
+        val constructor = kClass.constructors.single()
+        val parameters = constructor.parameters
+        val parameterValues = parameters.map {
+            val type = it.type
+
+            when (type) {
+                String::class.createType() -> "blah"
+                Int::class.createType() -> 1
+                else -> create(Class.forName(type.javaType.typeName).kotlin)
+            }
         }
+        return constructor.call(*parameterValues.toTypedArray())
     }
 
 }
