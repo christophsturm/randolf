eb77f79286580c13ad55adfb5ea6050666298d4c
Christoph Sturm <me@christophsturm.com>
2019-12-17 16:35:43 +0100

    dep bump

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,15 +6,15 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val junit5Version = "5.5.2"
 val junitPlatformVersion = "1.5.2"
-val kotlinVersion = "1.3.60"
+val kotlinVersion = "1.3.61"
 
 plugins {
     java
-    kotlin("jvm") version "1.3.60"
+    kotlin("jvm") version "1.3.61"
     id("com.github.ben-manes.versions") version "0.27.0"
     `maven-publish`
     id("com.jfrog.bintray") version "1.8.4"
-    id("info.solidsoft.pitest") version "1.4.5"
+    id("info.solidsoft.pitest") version "1.4.6"
 
 }
 
@@ -38,7 +38,7 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8", kotlinVersion))
     implementation(kotlin("reflect", kotlinVersion))
-    testImplementation("io.strikt:strikt-core:0.22.2")
+    testImplementation("io.strikt:strikt-core:0.23.2")
     testImplementation("dev.minutest:minutest:1.9.0")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junit5Version")
